7d3db9fd8a0fb1a2a8d5c115f90612fe4d6f9086
rlorv <eun040904@gmail.com>
2025-07-09 15:18:07 +0900

    Create code explanation_myWidget.py

---
+++ code explanation_myWidget.py
@@ -0,0 +1,25 @@
+1. myWidgets.py
+ㄴclass Widget1
+  ㄴ def __init__
+  ㄴ def get_data
+  ㄴ def set_data
+ㄴclass Widget2
+  ㄴ def __init__
+  ㄴ def get_data
+  ㄴ def set_data
+ㄴclass Widget3
+  ㄴ def __init__
+  ㄴ def get_data
+  ㄴ def set_data
+ㄴclass Widget31
+  ㄴ def __init__
+  ㄴ def get_data
+  ㄴ def set_data
+  ㄴ def create_exclusive_handler
+  ㄴ def get_selected_categories
+ㄴclass Widget32
+  ㄴ def __init__
+  ㄴ def get_data
+  ㄴ def set_data
+ㄴclass Widget4
+  ㄴ def __init__
